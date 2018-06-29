bbf8457357e456a7994c66655c434880f8694e1b
Jiří Vrbas <st39850@student.upce.cz>
2018-06-29 15:21:25 +0200

    Add project files.

---
+++ ChalangeYourself.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChalangeYourself.Website.MvcApplication" Language="C#" %>
---
+++ ChalangeYourself.Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChalangeYourself.Website.Startup))]
+namespace ChalangeYourself.Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
